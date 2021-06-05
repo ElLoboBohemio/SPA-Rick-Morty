4ababc548149d129091540d2cbd938685ec77473
Josué <josuelobbo@gmail.com>
2021-06-05 09:40:07 -0600

    indexjs con css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import router from './routes/index';
-import css from "./styles/style.css";
+import "./styles/style.css";
 
 window.addEventListener('load', router)
 window.addEventListener('hashchange', router);
